b0d6419bf555463a24309826bd2631ab39fc2dd8
Santiago Penino <santiago.penino@gmail.com>
2024-05-19 18:44:10 -0300

    otros detalles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,11 +4,11 @@ export default {
   theme: {
     extend: {
       colors: {
-        "custom-blue": "#008fd0",
-        "custom-black": "#171717",
-        "custom-gray": "#303030",
-        "custom-white": "#f1f1f1",
-        "custom-yellow": "#ffbe23",
+        "custom-blue": "#006494",
+        "custom-black": "#171614",
+        "custom-gray": "#061826",
+        "custom-white": "#DEE5E5",
+        "custom-yellow": "#FF9F1C",
       },
     },
   },
