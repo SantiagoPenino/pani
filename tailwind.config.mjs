aa984f890f27e296357d036b25de0ffaefd0752f
Santiago Penino <santiago.penino@gmail.com>
2024-05-19 04:50:37 -0300

    Home completo, falta revisar

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,6 +7,8 @@ export default {
         "custom-blue": "#008fd0",
         "custom-black": "#171717",
         "custom-gray": "#303030",
+        "custom-white": "#f1f1f1",
+        "custom-yellow": "#ffbe23",
       },
     },
   },
