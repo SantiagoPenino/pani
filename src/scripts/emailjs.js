795c75374b1d45c8907d82eedb41c7067f6388ac
Santiago Penino <santiago.penino@gmail.com>
2024-06-10 02:33:47 -0300

    mail

--- src/scripts/emailjs.js
+++ src/scripts/emailjs.js
@@ -1,21 +1,26 @@
 import emailjs from "@emailjs/browser";
 
 document.addEventListener("astro:page-load", () => {
-    emailjs.init(import.meta.env.PUBLIC_EMAILJS_PUBLIC_KEY);
-    document
-      .getElementById("contact-form")
-      .addEventListener("submit", (e) => {
-        e.preventDefault();
+  const contactForm = document.getElementById("contact-form");
 
-        emailjs
-          .sendForm(import.meta.env.PUBLIC_EMAILJS_SERVICE_ID, import.meta.env.PUBLIC_EMAILJS_TEMPLATE_ID, e.target)
-          .then(
-            () => {
-              console.log("Enviado");
-            },
-            (error) => {
-              console.log("Error:", error.text);
-            }
-          );
-      });
+  emailjs.init(import.meta.env.PUBLIC_EMAILJS_PUBLIC_KEY);
+  contactForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        import.meta.env.PUBLIC_EMAILJS_SERVICE_ID,
+        import.meta.env.PUBLIC_EMAILJS_TEMPLATE_ID,
+        e.target
+      )
+      .then(
+        () => {
+          console.log("Enviado");
+          contactForm.reset();
+        },
+        (error) => {
+          console.log("Error:", error.text);
+        }
+      );
   });
+});
