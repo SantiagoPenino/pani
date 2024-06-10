aff434e605877397b781d3de6e0f26f14538356b
Santiago Penino <santiago.penino@gmail.com>
2024-06-10 02:38:01 -0300

    ruta script

--- src/scripts/emailjs.js
+++ src/scripts/emailjs.js
@@ -4,23 +4,25 @@ document.addEventListener("astro:page-load", () => {
   const contactForm = document.getElementById("contact-form");
 
   emailjs.init(import.meta.env.PUBLIC_EMAILJS_PUBLIC_KEY);
-  contactForm.addEventListener("submit", (e) => {
-    e.preventDefault();
+  if (contactForm) {
+    contactForm.addEventListener("submit", (e) => {
+      e.preventDefault();
 
-    emailjs
-      .sendForm(
-        import.meta.env.PUBLIC_EMAILJS_SERVICE_ID,
-        import.meta.env.PUBLIC_EMAILJS_TEMPLATE_ID,
-        e.target
-      )
-      .then(
-        () => {
-          console.log("Enviado");
-          contactForm.reset();
-        },
-        (error) => {
-          console.log("Error:", error.text);
-        }
-      );
-  });
+      emailjs
+        .sendForm(
+          import.meta.env.PUBLIC_EMAILJS_SERVICE_ID,
+          import.meta.env.PUBLIC_EMAILJS_TEMPLATE_ID,
+          e.target
+        )
+        .then(
+          () => {
+            console.log("Enviado");
+            contactForm.reset();
+          },
+          (error) => {
+            console.log("Error:", error.text);
+          }
+        );
+    });
+  }
 });
