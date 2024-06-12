439ab724511e6f67a459e6c18125c2c99627ae4b
Santiago Penino <santiago.penino@gmail.com>
2024-06-12 00:42:23 -0300

    cambios en folders

--- src/scripts/emailjs.js
+++ src/scripts/emailjs.js
@@ -1,28 +1,21 @@
 import emailjs from "@emailjs/browser";
 
 document.addEventListener("astro:page-load", () => {
-  const contactForm = document.getElementById("contact-form");
+    emailjs.init(import.meta.env.PUBLIC_EMAILJS_PUBLIC_KEY);
+    document
+      .getElementById("contact-form")
+      .addEventListener("submit", (e) => {
+        e.preventDefault();
 
-  emailjs.init(import.meta.env.PUBLIC_EMAILJS_PUBLIC_KEY);
-  if (contactForm) {
-    contactForm.addEventListener("submit", (e) => {
-      e.preventDefault();
-
-      emailjs
-        .sendForm(
-          import.meta.env.PUBLIC_EMAILJS_SERVICE_ID,
-          import.meta.env.PUBLIC_EMAILJS_TEMPLATE_ID,
-          e.target
-        )
-        .then(
-          () => {
-            console.log("Enviado");
-            contactForm.reset();
-          },
-          (error) => {
-            console.log("Error:", error.text);
-          }
-        );
-    });
-  }
-});
+        emailjs
+          .sendForm(import.meta.env.PUBLIC_EMAILJS_SERVICE_ID, import.meta.env.PUBLIC_EMAILJS_TEMPLATE_ID, e.target)
+          .then(
+            () => {
+              console.log("Enviado");
+            },
+            (error) => {
+              console.log("Error:", error.text);
+            }
+          );
+      });
+  });
