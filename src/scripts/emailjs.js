048914f46a9dfdd9215740c422a16cf3cf25283b
Santiago Penino <santiago.penino@gmail.com>
2024-06-12 01:48:57 -0300

    formulario completo

--- src/scripts/emailjs.js
+++ src/scripts/emailjs.js
@@ -1,21 +1,54 @@
 import emailjs from "@emailjs/browser";
 
 document.addEventListener("astro:page-load", () => {
-    emailjs.init(import.meta.env.PUBLIC_EMAILJS_PUBLIC_KEY);
-    document
-      .getElementById("contact-form")
-      .addEventListener("submit", (e) => {
-        e.preventDefault();
+  emailjs.init(import.meta.env.PUBLIC_EMAILJS_PUBLIC_KEY);
 
-        emailjs
-          .sendForm(import.meta.env.PUBLIC_EMAILJS_SERVICE_ID, import.meta.env.PUBLIC_EMAILJS_TEMPLATE_ID, e.target)
-          .then(
-            () => {
-              console.log("Enviado");
-            },
-            (error) => {
-              console.log("Error:", error.text);
-            }
-          );
-      });
-  });
+  const form = document.getElementById("contact-form");
+  const messageWrapper = document.createElement("div");
+  if (form) {
+    messageWrapper.classList.add(
+      "message-wrapper",
+      "w-full",
+      "text-center",
+      "font-bold",
+      "uppercase",
+      "lg:text-nowrap",
+      "flex",
+      "justify-center",
+      "pt-6",
+      "text-xs",
+      "lg:text-base"
+    );
+    form.appendChild(messageWrapper);
+    const message = document.createElement("p");
+
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      emailjs
+        .sendForm(
+          import.meta.env.PUBLIC_EMAILJS_SERVICE_ID,
+          import.meta.env.PUBLIC_EMAILJS_TEMPLATE_ID,
+          e.target
+        )
+        .then(
+          () => {
+            console.log("Enviado");
+            const message = document.createElement("p");
+            message.textContent =
+              "Gracias por contactarnos. En breve nos pondremos en contacto contigo.";
+            message.classList.add("text-green-500");
+            messageWrapper.appendChild(message);
+            form.reset();
+          },
+          (error) => {
+            console.log("Error:", error.text);
+            message.textContent =
+              "Error al enviar el formulario: " + error.text;
+            message.classList.add("error", "text-red-500");
+            messageWrapper.appendChild(message);
+          }
+        );
+    });
+  }
+});
