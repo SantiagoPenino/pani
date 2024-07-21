8e4c6093aa12d00141bb0cc16309926a10f08410
Santiago Penino <santiago.penino@gmail.com>
2024-07-20 22:33:05 -0300

    probando gtag

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import dotenv from "dotenv";
 import tailwind from "@astrojs/tailwind";
-
+import partytown from "@astrojs/partytown";
 dotenv.config();
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), partytown()],
   build: {
-    minify: "esbuild",
-  },
+    minify: "esbuild"
+  }
 });
